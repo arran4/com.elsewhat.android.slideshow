a8a25dae0704691e53427e02c3617e6967c4b257
arran <arran4appengine2@arran4.com>
2013-01-13 17:25:03 +1100

    Somefiles missing.. Fork failed? Damn Intellij!?

---
+++ src/main/java/com/elsewhat/android/slideshow/backend/AllPhotoFiles.java
@@ -0,0 +1,22 @@
+package com.elsewhat.android.slideshow.backend;
+
+import android.content.Context;
+import com.elsewhat.android.slideshow.api.SlideshowPhoto;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: arran
+ * Date: 13/01/13
+ * Time: 5:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllPhotoFiles {
+    public AllPhotoFiles(String path) {
+
+    }
+    public List<SlideshowPhoto> getSlideshowPhotos(Context context) throws Throwable {
+
+    }
+}
