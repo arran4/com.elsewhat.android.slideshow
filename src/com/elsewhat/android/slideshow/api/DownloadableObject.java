0cbce0fcef89fbe12807565daaff1e01d9fe88f9
arran <arran4appengine2@arran4.com>
2013-01-13 17:19:05 +1100

    Initialized maven file. Actually gotta make it work now.

--- src/com/elsewhat/android/slideshow/api/DownloadableObject.java
+++
@@ -1,9 +0,0 @@
-package com.elsewhat.android.slideshow.api;
-
-public interface DownloadableObject {
-	public String getUrlStringForDownload();
-	public String getFileName();
-	public void setDownloadFailed(Throwable t, String message);
-	public boolean isDownloadFailed();
-	
-}
--- src/com/elsewhat/android/slideshow/api/SlideshowBackend.java
+++
@@ -1,9 +0,0 @@
-package com.elsewhat.android.slideshow.api;
-
-import java.util.List;
-
-import android.content.Context;
-
-public interface SlideshowBackend {
-	public List<SlideshowPhoto> getSlideshowPhotos(Context context) throws Throwable;
-}
